e81efd44ce71872197456a0628be7a1c89d85417
WalessonSG <walesson.sg@gmail.com>
2023-09-27 16:08:55 -0300

    ...

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -2,39 +2,69 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 import getMovie from "../../services/getMovie";
-import { Movie } from "../../@types/Movie";
 const API_KEY = import.meta.env.VITE_API_KEY;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const IMG = import.meta.env.VITE_IMG
 
 export default function Details() {
-  const[movie, setMovie] = useState<Movie>();
+  const[movie, setMovie] = useState();
   const [loading, setLoading] = useState(true);
-
+  const genres = []
   const { id } = useParams();
 
-  async function fetchMovie() {
+  async function fetchMovie(url:string) {
     try {
-      //linha para teste: 17
-      const { data } = await getMovie(`/3/movie/${3}?api_key=${API_KEY}`);
-      const { id, adult, genres, original_title, overview } = data;
-      setMovie({id, adult, genres, original_title, overview});
+      const { data } = await getMovie(url);
+      setMovie(data);
+
       setLoading(false);
     } catch(e){
-      console.log(e)
+      console.log(e);
     }
   }
 
+  const concatGenres = (genres:any) => {
+    let concat =''
+    for(let i in genres){
+      Number(i) < genres.length-1 ? concat += genres[i].name +", " : concat += genres[i].name
+    }
+    return concat
+  }
+
   useEffect(() => {
-    if (!movie) fetchMovie();
-  }, [movie]);
+    const movieURL = `${BASE_URL}/3/movie/${id}?api_key=${API_KEY}`;
+    fetchMovie(movieURL);
+
+  }, []);
 
   return (
     <div>
-      <h1>Details</h1>
-      <p>{movie?.id}</p> 
-      <p>{movie?.original_title}</p> 
-      <p>{movie?.overview}</p>
+      <div>
+      <img src={IMG+movie?.poster_path}/>
 
-      <p>{movie?.genres[0].name}</p>   
+      </div>
+
+      <div> 
+        <h1>{movie && <>{movie.title}</>}</h1>
+      </div>
+
+      <div> 
+        <h2>{movie && <>Nota: {movie.vote_average}</>}</h2>
+        <h2>{movie && <>N° votos: {movie.vote_count}</>}</h2>
+      </div>
+
+      <div>
+        <h2>Sinopse</h2> 
+        {movie && <h4>{movie.overview}</h4>}
+        {movie && <h4>{console.log(movie)}</h4>}
+      </div>
+      
+      <div>
+        <h2>Gêneros: {movie && <>{concatGenres(movie.genres)}</>}</h2> 
+      </div>
+
+
+
 
     </div>
     
