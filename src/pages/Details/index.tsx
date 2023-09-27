2d755097fcf30d3aecd7964ad8ca17cdeaf43f23
Naianna Duarte <121508548+naiannaduarte@users.noreply.github.com>
2023-09-27 14:01:54 -0300

    Update index.tsx
    Removendo linhas de comentários

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -73,7 +73,6 @@ export default function Details() {
           <MoviePageDiv>
             <MovieTitle>{movie?.original_title}</MovieTitle>
             <MoviePageDivImg>
-              // TODO: Colocar URL no .env // TODO: Centralizar image
               <MoviePageImg
                 src={`https://image.tmdb.org/t/p/w500/${movie?.poster_path}`}
               />
@@ -82,7 +81,6 @@ export default function Details() {
               Sinopse
               <MovieSinopseText>{movie?.overview}</MovieSinopseText>
             </MovieSinopse>
-            // TODO: Adicionar mais informações do file - Nova div
           </MoviePageDiv>
         </>
       )}
